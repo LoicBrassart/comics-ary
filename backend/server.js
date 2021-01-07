69fd80ed9b019b821da354635e803379a366d194
Loic <loic.brassart@gmail.com>
2021-01-07 14:52:09 +0100

    Add first two routes with MySQL results

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,29 @@
 const express = require("express");
 const app = express();
-
+const { db } = require("./conf")
 app.use(express.json())
 
-let comics = [{
-    author: "LBrassart",
-    title: "Wild Stories",
-    illustrator: "N/A",
-    year_start: 1970,
-    resume: "Lorem ipsum"
-},
-{
-    author: "Stan Lee",
-    title: "Batman",
-    illustrator: "N/A",
-    year_start: 1939,
-    resume: "Lorem bat-ipsum"
-}
-];
-
 app.get("/comics", (req, res) => {
-    res.json(comics);
+    db.query(`SELECT id, title, illustrator, author, year_start, resume, picture from comic`, (errSql, resSql) => {
+        if (errSql) {
+            return res.status(500).send("Nope, sorry, I iz broken !")
+        }
+        return res.json(resSql)
+    })
 })
 
 app.get("/comics/:id", (req, res) => {
     const id = req.params.id;
-    console.log(id);
-    res.json(comics[id]);
+    db.query(`SELECT id, title, illustrator, author, year_start, resume, picture from comic WHERE id=?`, [id], (errSql, resSql) => {
+        if (errSql) {
+            return res.status(500).send("Nope, sorry, I iz broken !")
+        }
+        if (!resSql.length) {
+            return res.status(500).send("Must be tired, found nothing :(")
+        }
+
+        return res.json(resSql)
+    })
 })
 
 app.post("/comics", (req, res) => {
