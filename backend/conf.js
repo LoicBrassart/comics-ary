5d35cf875872c2947ba0e2800d80f25e9773c639
Loic <loic.brassart@gmail.com>
2021-01-07 15:50:05 +0100

    Prettify

--- backend/conf.js
+++ backend/conf.js
@@ -1,14 +1,14 @@
-const mysql = require('mysql2');
-require('dotenv').config()
-const { DB_HOST, DB_PASS, DB_USER, DB_DATABASE } = process.env
+const mysql = require("mysql2");
+require("dotenv").config();
+const { DB_HOST, DB_PASS, DB_USER, DB_DATABASE } = process.env;
 
 const connection = mysql.createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    database: DB_DATABASE,
-    password: DB_PASS
+  host: DB_HOST,
+  user: DB_USER,
+  database: DB_DATABASE,
+  password: DB_PASS,
 });
 
 module.exports = {
-    db: connection
-}
+  db: connection,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,47 @@
 const express = require("express");
 const app = express();
-const { db } = require("./conf")
-app.use(express.json())
+const { db } = require("./conf");
+app.use(express.json());
 
 app.get("/comics", (req, res) => {
-    db.query(`SELECT id, title, illustrator, author, year_start, resume, picture from comic`, (errSql, resSql) => {
-        if (errSql) {
-            return res.status(500).send("Nope, sorry, I iz broken !")
-        }
-        return res.json(resSql)
-    })
-})
+  db.query(
+    `SELECT id, title, illustrator, author, year_start, resume, picture from comic`,
+    (errSql, resSql) => {
+      if (errSql) {
+        return res.status(500).send("Nope, sorry, I iz broken !");
+      }
+      return res.json(resSql);
+    }
+  );
+});
 
 app.get("/comics/:id", (req, res) => {
-    const id = req.params.id;
-    db.query(`SELECT id, title, illustrator, author, year_start, resume, picture from comic WHERE id=?`, [id], (errSql, resSql) => {
-        if (errSql) {
-            return res.status(500).send("Nope, sorry, I iz broken !")
-        }
-        if (!resSql.length) {
-            return res.status(500).send("Must be tired, found nothing :(")
-        }
+  const id = req.params.id;
+  db.query(
+    `SELECT id, title, illustrator, author, year_start, resume, picture from comic WHERE id=?`,
+    [id],
+    (errSql, resSql) => {
+      if (errSql) {
+        return res.status(500).send("Nope, sorry, I iz broken !");
+      }
+      if (!resSql.length) {
+        return res.status(500).send("Must be tired, found nothing :(");
+      }
 
-        return res.json(resSql)
-    })
-})
+      return res.json(resSql);
+    }
+  );
+});
 
 app.post("/comics", (req, res) => {
-    db.query(`INSERT INTO comic SET ?`, req.body, (errSql, resSql) => {
-        if (errSql) {
-            return res.status(500).send("Nope, sorry, I iz broken !")
-        }
-        res.status(201).send("Ok! Created new comics")
-    })
-})
+  db.query(`INSERT INTO comic SET ?`, req.body, (errSql, resSql) => {
+    if (errSql) {
+      return res.status(500).send("Nope, sorry, I iz broken !");
+    }
+    res.status(201).send("Ok! Created new comics");
+  });
+});
 
 app.listen(5050, () => {
-    console.log("Hey there !")
-})
+  console.log("Hey there !");
+});
