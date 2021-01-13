2eb7c165cb2e2f3bda14f454f5d86eb4696c92f1
Loic <loic.brassart@gmail.com>
2021-01-13 13:35:40 +0100

    Prettify

--- backend/generateFixtures.js
+++ backend/generateFixtures.js
@@ -1,16 +1,18 @@
-const faker = require('faker');
+const faker = require("faker");
 
 let query = `INSERT INTO comic (title, author, illustrator, year_start, resume, picture) VALUES `;
 const rows = [];
 faker.locale = "fr";
 
 for (let i = 0; i < 100; i++) {
-    const oneRow = faker.fake("('{{random.words}}', '{{name.firstName}} {{name.lastName}}', '{{name.firstName}} {{name.lastName}}', {{random.number}}, '{{lorem.sentences}}', '{{image.imageUrl}}') ");
-    rows.push(oneRow)
+  const oneRow = faker.fake(
+    "('{{random.words}}', '{{name.firstName}} {{name.lastName}}', '{{name.firstName}} {{name.lastName}}', {{random.number}}, '{{lorem.sentences}}', '{{image.imageUrl}}') "
+  );
+  rows.push(oneRow);
 }
-query += rows.join(",")
+query += rows.join(",");
 
-console.log(query)
+console.log(query);
 
 // If you wanna launch this script: node generateFixtures.js > fixtures.sql
 // Once it's generated, you can import it through the console, or copy/paste it's content in WBench
